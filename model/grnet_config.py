8524770cd399861814cadcefc41455bdb7306c5f
Raul Dominguez <rauldavidds@hotmail.com>
2023-06-30 12:08:10 +0200

    updates config and train

--- model/grnet_config.py
+++ model/grnet_config.py
@@ -1,14 +1,18 @@
 from src import train_grnet
 
 config = {
-    'experiment_name': '3_1_3depn_overfitting',
+    'experiment_name': 'GRNet_overfitting',
     'device': 'cuda:0',  # change this to cpu if you do not have a GPU
     'is_overfit': True,
     'batch_size': 32,
     'resume_ckpt': None,
-    'learning_rate': 0.001,
+    'learning_rate': 1e-4,
+    'weight_decay': 0,
+    'betas': (.9, .999),
     'max_epochs': 250,
     'print_every_n': 10,
     'validate_every_n': 25,
+    'milestones':[50],
+    'gamma': 0.5
 }
 train_grnet.main(config)
