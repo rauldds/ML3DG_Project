e5ecee94fee88435fde5064480fca3284199a565
Raul Dominguez <rauldavidds@hotmail.com>
2023-07-15 22:14:46 +0200

    filtering and separating parameters

--- config/grnet_config.py
+++ config/grnet_config.py
@@ -1,20 +1,21 @@
 config = {
-    'experiment_name': 'GRNet_comp_overfitting',
     'device': 'cuda:0',  # change this to cpu if you do not have a GPU (cuda:0)
     'is_overfit': False,
-    'batch_size': 50,
-    'batch_size_val': 64,
-    'resume': False,
+    'num_workers': 3,
+    'save_freq': 10,
+    'batch_size': 24,
+    'batch_size_val': 24,
+    'max_epochs': 21,
+    'validate_every_n': 430,
     'learning_rate': 0.002,
     'weight_decay': 1e-4,
     'betas': (.9, .999),
-    'max_epochs': 300,
-    'print_every_n': 10,
-    'validate_every_n': 500,
     'milestones':[40, 80, 120, 200],
-    'num_workers':8,
     'gamma': 0.5,
-    'save_freq': 10
+    "cls_net":{
+        'learning_rate': 0.0001,
+        'weight_decay': 1e-4,
+    }
 }
 completion_layers = ["conv1", "conv2", "conv3", "conv4", "fc5", "fc6",
                      "dconv7","dconv8","dconv9", "dconv10"]
